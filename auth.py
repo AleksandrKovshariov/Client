2b5af890e13877b60f9d661494943f012d94245b
Reneska <Reneska@ex.ua>
2019-04-01 18:16:51 +0300

    Temp fix

--- auth.py
+++ auth.py
@@ -5,7 +5,7 @@ import functools
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
 from auth_settings import AUTH_PATH, CLIENT_ID, CLIENT_SECRET
-from files import render_error
+# from files import render_error
 
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -28,7 +28,8 @@ def login():
             return render_template('service_not_available.html', message="Can't send a request to the server")
 
         if req.status_code != 200:
-            return render_error(req)
+            return render_template('service_not_available.html', message="Service error")
+            # return render_error(req)
 
         contents = json.loads(req.text)
 
