4390a979ce0c4df0bdcc4637e6e7f2d641e096d0
Reneska <Reneska@ex.ua>
2019-04-12 13:41:25 +0300

    Change filters design, move it to top and add disable for uploading button

--- static/js/upload.js
+++ static/js/upload.js
@@ -3,6 +3,7 @@ $(function () {
 
     const progress_bar = $('.progress-bar');
     const status = $('#status');
+    const btn_submit = $('#submit');
 
     $('#upload_form').ajaxForm({
         beforeSend: function() {
@@ -10,6 +11,8 @@ $(function () {
             let percentVal = '0%';
             progress_bar.width(percentVal);
             progress_bar.html(percentVal);
+            btn_submit.val("Loading...");
+            btn_submit.prop('disabled', true);
         },
         uploadProgress: function(event, position, total, percentComplete) {
             let percentVal = percentComplete + '%';
@@ -18,6 +21,8 @@ $(function () {
         },
         complete: function(xhr) {
             status.html(xhr.responseText);
+            btn_submit.val("Upload");
+            btn_submit.prop('disabled', false);
         }
     });
 });
